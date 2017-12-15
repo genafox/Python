0f0909ea70523ec05611987194ff3fc86040d42d
Gennadiy Falatiuk <gena.fox.gf@gmail.com>
2017-12-15 15:44:33 +0200

    Default template works

---
+++ Labs/Lab5/env_lab5/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!D:\DEVELOPMENT\Repositories\GIT-GF\Python\Labs\Lab5\env_lab5\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
